b4b87d5e63a1d46d80b0b95daa84733b59f05dd5
Blerinë <blerineberisha@gmail.com>
2022-10-14 19:55:21 +0200

    BE: created folders, started with user DTO

---
+++ m151.project/src/main/java/berisha/project/DTOs/Users.java
@@ -0,0 +1,9 @@
+package berisha.project.DTOs;
+
+public class Users {
+    private String id;
+    private String username;
+    private String address;
+    private String firstname;
+    private String lastname;
+}
